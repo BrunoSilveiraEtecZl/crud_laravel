41ef7cd7a3053aae4d8685d8db271c86201c0fbb
BrunoSilveiraEtecZl <bruno.dionisio@etec.sp.gov.br>
2022-05-20 16:13:39 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD - MENU</title>
+</head>
+
+<body>
+    <H1>CRUD LARAVEL</h1>
+   <a href="/cadastrar">cadastrar &nbsp;&nbsp;
+       <a href="/listar-produto/1">Listar 1 &nbsp;&nbsp;
+           <a href="editar-produto/1">Editar 1 &nbsp;&nbsp;
+               <a href="/excluir-produto/1">Excluir 1 &nbsp;&nbsp;
+
+</body>
+
+</html>
